4279ff77ff804d651b993e2db4ee38da3a1c118e
Steve Kudirka <skudirka@lakana.com>
2019-08-19 18:06:17 -0500

    Integrated Redux Thunk to expose dispatch to selectors and perform async (AJAX) functions within selectors. Using Container Pattern to refactor loading state to containers instead of Shop component.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,68 +2,32 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import {connect} from 'react-redux';
 
-import {updateCollections} from '../../redux/shop/shot.actions';
+import {fetchCollectionsStartAsync} from '../../redux/shop/shot.actions';
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.components';
-import CollectionPage from '../collection/collection.component';
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
-
-import {firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utils';
-
-const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
+import CollectionPageContainer from '../collection/collection.container';
 
 class ShopPage extends React.Component {
-
-    state = {
-        loading: true
-    };
-
-    unsubscribeFromSnapshot = null;
     
     componentDidMount() {
-        const {updateCollections} = this.props;
-        const collectionRef = firestore.collection('collections');
-        
-        // Observer/Observable Pattern
-        /*this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
-            const collectionsMap = convertCollectionsSnapshotToMap( snapshot );
-            updateCollections( collectionsMap );
-            this.setState({loading: false});
-        });*/
+        const { fetchCollectionsStartAsync } = this.props;
 
-        // Promise Pattern
-        collectionRef.get()
-            .then(snapshot => {
-                const collectionsMap = convertCollectionsSnapshotToMap( snapshot );
-                updateCollections( collectionsMap );
-                this.setState({loading: false});
-            });
-
-        // Native Fetch API Pattern
-        /*fetch('https://firestore.googleapis.com/v1/projects/crwn-db-8f52a/databases/(default)/documents/collections')
-            .then(response => response.json())
-            .then(collections => console.log('collections: ', collections));*/
+        fetchCollectionsStartAsync();
     }
-
-    /*componentWillUnmount(){
-        this.unsubscribeFromSnapshot();
-    }*/
 
     render() {
         const {match} = this.props;
-        const {loading} = this.state;
         return (
             <div className="shop-page">
-                <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />} />
-                <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />} />
+                <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
+                <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
             </div>
         )
     }
 }
 
 const mapDispatchToProps = dispatch => ({
-    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
